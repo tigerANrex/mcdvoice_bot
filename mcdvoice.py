4822ba427a8f7f49f8beefabf02d7c46c472eeaf
tigerANrex <nguyen11@memphis.edu>
2018-07-09 02:08:55 -0500

    It Works

--- mcdvoice.py
+++ mcdvoice.py
@@ -18,20 +18,25 @@ browser.fill('CN5', survey_code[20:25])
 browser.fill('CN6', survey_code[25])
 browser.find_by_name('NextButton').click()
 
-spans = browser.find_by_tag("span")
-button = []
-count = 0
-for i in spans:
-    if i.has_class('radioButtonHolder'):
-        button.append(i)
-        # if i == button[len(button)-1]:                        Get this tow rok please
-        #     print("yes")
-        #     button[randint(0, len(button)-1)].click()
-    elif i.has_class('radioBranded'):
-        button.append(i)
-        count += 1
-        if count == 5:
-            button[randint(0, 4)].click()
-            count = 0
-
-browser.find_by_id('NextButton').click()
+while True:
+    spans = browser.find_by_tag("span")
+    button = []
+    count = 0
+    for i in spans:
+        # if i.has_class('radioButtonHolder'):
+        #     button.append(i)
+        if i.has_class('radioBranded'):
+            button.append(i)
+            count += 1
+            if count == 5:
+                button[randint(0, len(button)-1)].click()
+                count = 0
+                button = []
+    try:
+        if len(button) < 5 and len(button) > 0:
+            button[randint(0, len(button)-1)].click()
+        browser.find_by_id('NextButton').click()
+    except:
+        if browser.is_element_not_present_by_id('finishForm'):
+            break
+        # browser.fill('S081000','Aye that\'s pretty good. The drive thru wait wasn\'t too long, and person who took my order spoke clearly and got my order correctly. When I got my food it was warm and the order was correct.')
